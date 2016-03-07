ed2addc3ffbe3c17056ee466bd1c4793f2a78269
itmightgetloud <ksnopek@gmail.com>
2016-03-07 20:51:31 +0100

    almost done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,72 @@
 $(document).ready(function(){
   createBoard(16);
+  showGrid();
+  hoverBlue();
+  $('#inlineRadio2').click(function(){
+    clearBoard();
+    hoverBlue();
+  });
+  $('#inlineRadio1').click(function(){
+    clearBoard();
+    hoverRandom();
+  });
+  $('.btn, #pixel-width').click(function(){
+    clearBoard();
+  });
 });
 
 function createBoard (size){
-  var tab = [];
-  for(var i = 1; i < size * size; i++){
-    tab[i] = "<div class='square'></div>";
+  for(var i = 0; i < size * size; i++){
+    $(".board").append("<div class='square'></div>");
   }
-  $(".board").append(tab.join(''));
 }
+function hoverRandom (){
+  $('.square').mouseenter(function(){
+      $(this).css('background', 'rgb(' + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')');
+    });
+  $('.square').mouseleave(function(){
+      $(this).css('background', 'rgba(255,255,255,.7)');
+    });
+}
+function hoverBlue (){
+  $('.square').mouseenter(function(){
+    $(this).addClass('hoverBlue');
+    });
+  $('.square').mouseleave(function(){
+    $(this).removeClass('hoverBlue');
+    });
+}
+
+function clearBoard(){
+    do{
+      var getSize = prompt("Please, enter the size of squares:");
+      if(getSize > 100)
+        alert('Please enter size of square < 100:');
+      else{
+        if (!isNaN(getSize)){
+          $(".board").empty();
+          createBoard(Math.round(getSize));
+        };
+
+        var squareSize = 600/getSize;
+        var squareStyle = {
+        width: squareSize + 'px',
+        height: squareSize + 'px'
+        };
+
+        $('.square').css(squareStyle);
+      }
+    }while(getSize > 100);
+}
+
+function showGrid(){
+  $('#show-grid').click(function(){
+    $('.square').toggleClass('dupa');
+  });
+
+}
+
+/*
+do zrobienia:
+-zostawianie koloru (i dodanie przyciksu)
+-zapamietanie ustawien po kliknieciu "reset"
